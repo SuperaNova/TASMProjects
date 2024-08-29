1bbaf218af2d7be1591c3bc54f070bc3708fbec1
SuperaNova <52492619+SuperaNova@users.noreply.github.com>
2024-08-29 09:09:13 -0700

    Improved Exer1 code

---
+++ Exer1pro.asm
@@ -0,0 +1,54 @@
+.model small
+.stack 100h
+
+.code
+main proc
+
+    mov bl, 'a'
+    
+LowerAZ_loop:
+    ; Print the char in BL
+    mov ah, 02h
+    mov dl, bl   
+    int 21h   
+
+    ; Move to the next character
+    inc bl         
+
+    ; Check if reached Z
+    cmp bl, 'z' + 1  ; Compare BL with Z + 1
+    jb LowerAZ_loop    ; If BL < Z + 1, continue
+
+    
+    ; Set to A
+    mov bl, 'A'
+
+    
+    mov ah, 02h
+    mov dl, 0ah     ; newline
+    int 21h 
+    
+UpperAZ_loop:
+    ; Print the char in BL
+    mov ah, 02h
+    mov dl, bl   
+    int 21h   
+
+    ; Move to the next character
+    inc bl         
+    
+    ; space
+    mov ah, 02h
+    mov dl, ' '
+    int 21h
+    
+    ; Check if reached Z
+    cmp bl, 'Z' + 1  ; Compare BL with 'Z' + 1
+    jb UpperAZ_loop    ; If BL < 'Z' + 1, continue
+    
+    ; Terminate the program
+    mov ax, 4C00h
+    int 21h
+
+main endp
+end main
